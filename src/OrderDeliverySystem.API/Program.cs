bb15e41b78b42c74f92bf1665ec6e222ae6749d7
DESKTOP-CHI1TLF\BOT <ebumaurice@gmail.com>
2026-04-23 01:10:25 +0100

    Initial solution setup with gitignore and README files

---
+++ src/OrderDeliverySystem.API/Program.cs
@@ -0,0 +1,30 @@
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// For DI (we’ll plug this in soon)
+// builder.Services.AddApplication();
+// builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+app.Run();
